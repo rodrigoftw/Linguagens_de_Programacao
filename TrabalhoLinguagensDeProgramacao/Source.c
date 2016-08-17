a90bcb7794736e5e7a54d851491a1a672fbe0226
Fabiano <fabianoabreu90@outlook.com>
2016-08-17 15:19:16 -0300

    criacao parcial dos menus

--- TrabalhoLinguagensDeProgramacao/Source.c
+++ TrabalhoLinguagensDeProgramacao/Source.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "libaluno.h"
 #include "libprofessor.h"
 #include "libdisciplina.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
 
 int main() 
 {
+	setlocale(LC_ALL, "Portuguese");
+
 	listaDeAlunos listaAluno;
 	listaAluno.inicio = NULL;
 	listaDeProfessores listaProfessor;
@@ -13,7 +17,122 @@ int main()
 	listaDeDisciplinas listaDisciplina;
 	listaDisciplina.inicio = NULL;
 
-	printf("-------------------- CADASTRO --------------------");
+	int matricula;
+	char nome[50];
+	int opcao;
+
+	do 
+	{
+		printf("\n");
+		printf("------------------------------ MENU PRINCIPAL ------------------------------\n");
+		printf("\t1 - INSERIR\n");
+		printf("\t2 - EDITAR\n");
+		printf("\t3 - EXIBIR\n");
+		printf("\t4 - EXCLUIR\n");
+		printf("\t0 - SAIR\n");
+		printf("----------------------------------------------------------------------------\n");
+		
+		do 
+		{
+			printf("\tESCOLHA A OP플O DESEJADA: ");
+			scanf("%d", &opcao);
+
+			switch (opcao)
+			{
+			case 1:
+				system("cls");
+				printf("\n");
+				printf("------------------------------- MENU INSERIR -------------------------------\n");
+				printf("\t1 - INSERIR ALUNO\n");
+				printf("\t2 - INSERIR PROFESSOR\n");
+				printf("\t3 - INSERIR DISCIPLINA\n");
+				printf("\t4 - INSERIR TURMA\n");
+				printf("\t0 - VOLTAR AO MENU PRINCIPAL\n");
+				printf("----------------------------------------------------------------------------\n");
+				printf("\tESCOLHA A OP플O DESEJADA: ");
+				scanf("%d", &opcao);
+				
+				switch (opcao)
+				{
+				case 1:
+					system("cls");
+					printf("\n");
+					printf("------------------------------- INSERIR ALUNO -------------------------------\n");
+					printf("\tINFORME A MATRICULA E O NOME DO ALUNO...\n");
+					printf("\tMATRICULA: ");
+					scanf("%d", &matricula);
+					printf("\tNOME: ");
+					scanf("%s", nome);
+					inserirAluno(&listaAluno, matricula, nome);
+				default:
+					break;
+				}
+				system("cls");
+				break;
+			case 2:
+				system("cls");
+				printf("\n");
+				printf("------------------------------- MENU EDITAR --------------------------------\n");
+				printf("\t1 - EDITAR ALUNO\n");
+				printf("\t2 - EDITAR PROFESSOR\n");
+				printf("\t3 - EDITAR DISCIPLINA\n");
+				printf("\t4 - EDITAR TURMA\n");
+				printf("\t0 - VOLTAR AO MENU PRINCIPAL\n");
+				printf("----------------------------------------------------------------------------\n");
+				printf("\tESCOLHA A OP플O DESEJADA: ");
+				scanf("%d", &opcao);
+				//CONTINUAr
+				//...
+				system("cls");
+				break;
+			case 3:
+				system("cls");
+				printf("\n");
+				printf("------------------------------- MENU EXIBIR --------------------------------\n");
+				printf("\t1 - EXIBIR ALUNO\n");
+				printf("\t2 - EXIBIR PROFESSOR\n");
+				printf("\t3 - EXIBIR DISCIPLINA\n");
+				printf("\t4 - EXIBIR TURMA\n");
+				printf("\t0 - VOLTAR AO MENU PRINCIPAL\n");
+				printf("----------------------------------------------------------------------------\n");
+				printf("\tESCOLHA A OP플O DESEJADA: ");
+				scanf("%d", &opcao);
+				//CONTINUAR
+				//...
+				system("cls");
+				break;
+			case 4:
+				system("cls");
+				printf("\n");
+				printf("------------------------------- MENU EXCLUIR -------------------------------\n");
+				printf("\t1 - EXCLUIR ALUNO\n");
+				printf("\t2 - EXCLUIR PROFESSOR\n");
+				printf("\t3 - EXCLUIR DISCIPLINA\n");
+				printf("\t4 - EXCLUIR TURMA\n");
+				printf("\t0 - VOLTAR AO MENU PRINCIPAL\n");
+				printf("----------------------------------------------------------------------------\n");
+				printf("\tESCOLHA A OP플O DESEJADA: ");
+				scanf("%d", &opcao);
+				//CONTINUAR
+				//...
+				system("cls");
+				break;
+			case 0:
+				system("cls");
+				break;
+			default:
+				printf("\n");
+				printf("\tOP플O INV핶IDA\n");
+				opcao = 0;
+				system("pause");
+				system("cls");
+				break;
+			}
+		} while (opcao != 0);
+		
+	} while (opcao != 0);
+	
+	system("pause");
 
 
 
